35bfcbcb2bb3671f91da9ae5a093a48a82e8c61f
Dineshkuna <kunadinesh.work@gmail.com>
2026-01-28 18:57:22 +0530

    error handling code inprogress

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,15 @@ import dotenv from 'dotenv';
 import { connectMongoDatabase } from './config/db.js';
 dotenv.config({path:"backend/config/config.env"});
 
+
 connectMongoDatabase();
+
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+})
+
 const port = process.env.PORT || 3000;
 
 
