10071cbb69dec42df2720d18c1b9204451d71d2c
DineshKuna <kunadinesh.work@gmail.com>
2026-02-08 15:55:08 +0530

    pagination is done

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../pageStyles/Products.css'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
@@ -7,24 +7,29 @@ import Product from '../components/Product';
 import PageTitle from '../components/PageTitle';
 import { getProduct, removeErrors } from '../features/products/productSlice';
 import Loader from '../components/Loader';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import NoProducts from '../components/NoProducts';
+import Pagination from '../components/Pagination';
+
 
    
 
 function Products() {
-    const  { loading,error, products} = useSelector((state) => state.product);
+  const navigate = useNavigate();
+    const  { loading,error, products,resultsPerPage,productCount} = useSelector((state) => state.product);
     const dispatch = useDispatch();
    const location=  useLocation();
    const searchParams =  new URLSearchParams(location.search);
    const keyword = searchParams.get('keyword') || '';
     console.log(keyword);
+    const pageFromURL = parseInt(searchParams.get('page'),10) || 1;
+    const [currentPage, setCurrentPage] = useState(pageFromURL);
     
    console.log(searchParams)
     useEffect(() => {
-      dispatch(getProduct({keyword}));
-      }, [dispatch, keyword]);
+      dispatch(getProduct({keyword,page:currentPage}));
+      }, [dispatch, keyword,currentPage]);
 
       
        useEffect(() => {
@@ -33,6 +38,20 @@ function Products() {
             dispatch(removeErrors());
           }
         },[dispatch, error])
+
+
+        const handlePageChange = (page) => {
+          if (page !== currentPage) {
+            setCurrentPage(page);
+            const newSearchParams = new URLSearchParams(location.search);
+            if(page === 1){
+              newSearchParams.delete('page');
+            }else{
+              newSearchParams.set('page', page);
+            }
+            navigate(`?${newSearchParams.toString()}`);
+          }
+        }
 
   return (
     <>
@@ -50,6 +69,7 @@ function Products() {
             {products.length>0 ? (<div className="products-product-container">
              {products.map(( product,index) => (< Product  product={ product} key={index}/>))}
             </div>):(<NoProducts keyword={keyword}/>)}
+            <Pagination currentPage={currentPage} onPageChange={handlePageChange} />
         </div>
     </div>
 
@@ -61,5 +81,6 @@ function Products() {
     </>
   )
 }
+
 
 export default Products
